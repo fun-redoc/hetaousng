db29c9c017d0f7dac6f50d6d29b1084758c3a2a4
fun-redoc <fun.redoc@gmail.com>
2016-06-05 22:21:34 +0200

    initial

--- src/main/resources/static/app/controllers.js
+++ src/main/resources/static/app/controllers.js
@@ -1,7 +1,23 @@
 (function(angular) {
 "use strict";
-  var deleteCustomer = function(customer) {
-    console.log("delete customer", customer);
+  var deleteCustomer = function($scope, $http) {
+    console.log("delete customer", this, $http);
+    $http({method:'DELETE', url: this._links.self.href})
+      .success(function(data){
+        // TODO notify user of success
+        var idx = $scope.customers.indexOf(this);
+        console.log("success deleting data",$scope.customers, idx, data);
+        $scope.customers.splice(idx,1);
+      }.bind(this).bind(this.bind(this)))
+      .error(function(error) {
+        // TODO notify user of error
+        console.log("error", error);
+        $scope.error = error;
+      })
+  }
+
+  var updateCustomer = function($http) {
+    console.log("update customer", this);
   }
 
   angular.module("myApp.controllers")
@@ -12,7 +28,8 @@
         .success(function(data){
           angular.forEach(data._embedded.customers, 
                           function(customer){
-                            customer.delete = deleteCustomer.bind(customer);
+                            customer.delete = deleteCustomer.bind(customer, $scope, $http);
+                            customer.update = updateCustomer.bind(customer,$http);
                             $scope.customers.push(customer);
                           }); 
         }) 

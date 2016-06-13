0415fdaa0cd72c316132b3665773e4bcf8e36b32
fun-redoc <fun.redoc@gmail.com>
2016-06-13 23:47:34 +0200

    angular adapted

--- src/main/resources/static/app/controllers.js
+++ src/main/resources/static/app/controllers.js
@@ -1,13 +1,13 @@
 (function(angular) {
 "use strict";
-  var deleteCustomer = function($scope, $http) {
-    console.log("delete customer", this, $http);
+  var deletePerson = function($scope, $http) {
+    console.log("delete person", this, $http);
     $http({method:'DELETE', url: this._links.self.href})
       .success(function(data){
         // TODO notify user of success
-        var idx = $scope.customers.indexOf(this);
-        console.log("success deleting data",$scope.customers, idx, data);
-        $scope.customers.splice(idx,1);
+        var idx = $scope.persons.indexOf(this);
+        console.log("success deleting data",$scope.persons, idx, data);
+        $scope.persons.splice(idx,1);
       }.bind(this).bind(this.bind(this)))
       .error(function(error) {
         // TODO notify user of error
@@ -16,24 +16,28 @@
       })
   }
 
-  var updateCustomer = function($http) {
-    console.log("update customer", this);
+  var updatePerson = function($http) {
+    console.log("update person", this);
+  }
+
+  var addPerson = function($http) {
+    console.log("add person", this);
   }
 
   angular.module("myApp.controllers")
     .controller("AppController", function($scope, $http, $location) {
       console.log("AppController");
-      $scope.customers = [];
-      $scope.addCustomer = addCustomer.bind($scope);
-      $http({method:'GET', url    : "/api/customers"})
+      $scope.persons = [];
+      $scope.addPerson = addPerson.bind($scope);
+      $http({method:'GET', url    : "/api/persons"})
         .success(function(data){
-          angular.forEach(data._embedded.customers, 
-                          function(customer){
-                            customer.delete = deleteCustomer.bind(customer, $scope, $http);
-                            customer.update = updateCustomer.bind(customer,$http);
-                            $scope.customers.push(customer);
+          angular.forEach(data._embedded.persons, 
+                          function(person){
+                            person.delete = deletePerson.bind(person, $scope, $http);
+                            person.update = updatePerson.bind(person,$http);
+                            $scope.persons.push(person);
                           }); 
         }) 
         .error(function(error) { $scope.error = error})
-    });
+ });
 }(angular));
